b9d2553f8eedd5a7b3a9550b968482815dccc150
rexrex600 <jdavies0703@hotmail.co.uk>
2014-11-29 09:21:31 +0000

    Added Threaded Generation Code
    With worker pool

--- Perlin Noise Testing/Threaded Code/PerlinColourLatestWithPoolThreading.py
+++ Perlin Noise Testing/Threaded Code/PerlinColourLatestWithPoolThreading.py
@@ -11,8 +11,10 @@
 import random
 import math
 from PIL import Image
+import multiprocessing
+import time
 
-img_size = 512
+img_size = 256
 
 def randomize():
     perm = range(256)
@@ -41,29 +43,62 @@ def fBm(x, y, per, octs, perm, dirs):
         val += 0.5**o * noise(x*2**o, y*2**o, per*2**o, perm, dirs)
     return val
 
-def gen_tile(frequency,tile_no):                                                         #My hackery from here
+def tile_gen(frequency):
     perm, dirs = randomize()
     frequency = float(frequency)
     size, freq, octs, data = img_size, 1/frequency, 5, []
     for y in range(size):
         for x in range(size):
             data.append(fBm(x*freq, y*freq, int(size*freq), octs, perm, dirs))
+    return data
+
+
+
+def gen_tile(tile_no):                                                      #My hackery from here
+    frequency = 100
+    data = tile_gen(frequency)
+    size = img_size
+
+    #forest_data = tile_gen(frequency)
+
     im = Image.new("L", (size, size))
     im.putdata(data, 128, 128)
     im.save("Tile%d.png"%tile_no)
-    x = im.getdata()
-    x = list(x)
     im1 = Image.new("RGB", (size, size))
     data1 = []
-    for i in x:
+    del data[:]
+    data = list(im.getdata())
+    for i in data:
         if i < 110:
-            data1.append((0,i, 255))
-        elif i >= 110 and i < 180:
-            data1.append((0,i,0))
+            data1.append((0,int(i), 255))
+        elif i >= 180:
+            data1.append((int(i),int(i),int(i)))
+        elif i >= 110 and i < 115:
+            data1.append((255,255,0))
         else:
-            data1.append((255,0,0))
+            a = random.randint(0,2)
+            if a == 0:
+                data1.append((0,int(i/2),0))
+            else:
+                data1.append((0,int(i),0))
     im1.putdata(data1, 128, 128)
-    im1.save("Tile%dcoloured.png"%tile_no)
+    im1.save("Tileb%dcoloured.png"%tile_no)
 
-for i in range(1):
-    gen_tile(64.0,i)
+
+start = time.time()
+
+if __name__ == '__main__':
+    active_process_list = multiprocessing.Queue()
+    pool = multiprocessing.Pool(processes=10)
+    pool.map(gen_tile,range(100))
+    pool.close()
+    pool.join()
+
+
+##for i in range(1):
+##    gen_tile(i)
+
+
+end = time.time()
+
+print end - start
